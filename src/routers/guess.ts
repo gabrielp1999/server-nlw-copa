e07070e14b9c2c427a58c65db6ee136da79a70a8
Gabriel <gabriel.gsm2017@gmail.com>
2022-12-18 17:18:03 -0300

    ajustes rotar /pools/:poolId/games/:gameId/guesses

--- src/routers/guess.ts
+++ src/routers/guess.ts
@@ -90,4 +90,86 @@ export async function guessRoutes(fastify: FastifyInstance) {
       return replay.status(201).send();
     }
   );
+
+  // fastify.post(
+  //   "/pools/:poolId/games/:gameId/guesses",
+  //   {
+  //     onRequest: [authenticate],
+  //   },
+  //   async (request, replay) => {
+  //     const guessParams = z.object({
+  //       poolId: z.string(),
+  //       gameId: z.string(),
+  //     });
+
+  //     const createGuessBody = z.object({
+  //       firstTeamPoints: z.number(),
+  //       secondTeamPoints: z.number(),
+  //     });
+
+  //     const { poolId, gameId } = guessParams.parse(request.params);
+  //     const { firstTeamPoints, secondTeamPoints } = createGuessBody.parse(
+  //       request.body
+  //     );
+
+  //     const participant = await prisma.participant.findUnique({
+  //       where: {
+  //         userId_poolId: {
+  //           poolId,
+  //           userId: request.user.sub,
+  //         },
+  //       },
+  //     });
+
+  //     if (!participant) {
+  //       return replay.status(400).send({
+  //         message: "Your're not allowed to create a guess inside this pool.",
+  //       });
+  //     }
+
+  //     const guess = await prisma.guess.findUnique({
+  //       where: {
+  //         participantId_gameId: {
+  //           participantId: participant.id,
+  //           gameId,
+  //         },
+  //       },
+  //     });
+
+  //     if (guess) {
+  //       return replay.status(400).send({
+  //         message: "You already send a guess to this game on this pooll.",
+  //       });
+  //     }
+
+  //     const game = await prisma.game.findUnique({
+  //       where: {
+  //         id: gameId,
+  //       },
+  //     });
+
+  //     if (!game) {
+  //       return replay.status(400).send({
+  //         message: "Game not found!",
+  //       });
+  //     }
+
+  //     if (game.date < new Date()) {
+  //       return replay.status(400).send({
+  //         message: "You cannot send guesses after game date.",
+  //       });
+  //     }
+
+  //     await prisma.guess.create({
+  //       data: {
+  //         gameId,
+  //         participantId: participant.id,
+  //         firstTeamPoints,
+  //         secondTeamPoints,
+  //       },
+  //     });
+
+  //     return replay.status(201).send();
+  //   }
+  // );
 }
